73f54d88fbca7c81ea6a1d183d735caa0e0ec27d
Richard <rcoric03@gmail.com>
2023-02-09 14:41:52 +0000

    :art: Implemented Card Class

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -6,13 +6,50 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223
 {
+    public enum SuitType
+    {
+        Clubs = 1,
+        Diamonds = 2,
+        Hearts = 3,
+        Spades = 4,
+    }
     class Card
     {
         //Base for the Card class.
         //Value: numbers 1 - 13
         //Suit: numbers 1 - 4
         //The 'set' methods for these properties could have some validation
-        public int Value { get; set; }
-        public int Suit { get; set; }
+        private SuitType suitValue;
+        private int cardValue;
+
+        public int Value
+        {
+            get
+            {
+                return cardValue;
+            }
+
+            set
+            {
+                if (value <= 13 && value >= 1)
+                {
+                    cardValue = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Value invalid");
+                }
+            }
+        }
+        public SuitType Suit {
+            get
+            {
+                return suitValue;
+            }
+            set
+            {
+                suitValue = value;         
+            }
+        }
     }
 }
